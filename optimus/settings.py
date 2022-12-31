f035c1dc803166a3e3b419dc2cd1be8e1eea4b57
Onwuka David <onwukadavid21@gmail.com>
2022-12-31 03:37:27 +0100

    added whitenoise

--- optimus/settings.py
+++ optimus/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',#
+
     'accounts',
     'builder',
     'crispy_forms',
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",#
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
